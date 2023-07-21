67c1df6d8f945a8d335331218b7339b89c3fa128
Nicholas Gong <38301069+ntgong90@users.noreply.github.com>
2023-07-20 23:33:46 -0700

    add main files

---
+++ index.js
@@ -0,0 +1,36 @@
+//set up local memory array of output words
+const vulgarWords = ['fuck you', 'bitch','cunt','motherfucker','shitstain','ogre','limpdick','gumby','paperhanded bitch','ass whipe', 'plonker'],
+	censoredWords = [],
+	outputWords = [];
+
+//push to new output array randomly
+
+function generateWords(numberOfWords){
+	for(let i = 0; i < numberOfWords; i++){
+		let randomIndex = Math.floor(Math.random()*vulgarWords.length);
+		outputWords.push(vulgarWords[randomIndex]);
+	}
+	console.log(outputWords)
+	return outputWords;
+}
+
+generateWords(20)
+
+
+
+
+
+
+
+//shuffler using Durstenfeld shuffle
+// function shuffleWords(words){
+// 	for(let i = words.length - 1; i > 0; i--){
+// 		const j = Math.floor(Math.random()*(i +1));
+// 		[words[i], words[j]] = [words[j], words[i]];
+// 	}
+// 	console.log(words)
+// 	return words;
+// }
+
+//shuffleWords(vulgarWords)
+
